56ec34581a35bf8596cb494b9c1ecb69fddde61c
D4rksoon <Asdreem2539@gmail.com>
2023-03-01 22:40:57 +0800

    Добавление 2-х функций
    1) Создание случайного массива в файле
    2) Проверка на упорядоченность массива в файле

--- NaturalMergeSortOnFourFiles/NaturalMergeSortOnFourFiles/NaturalMergeSortOnFourFiles.cpp
+++ NaturalMergeSortOnFourFiles/NaturalMergeSortOnFourFiles/NaturalMergeSortOnFourFiles.cpp
@@ -1,6 +1,40 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <ios>
+
+
+bool createFileWithRandNum(const std::string& fileName, const int numCount, const int maxNum)
+{
+	std::ofstream file(fileName);
+	if (!file.is_open())
+		return false;
+	for (int i = 0; i < numCount; i++)
+	{
+		int x = rand() % maxNum;
+		file << x << ' ';
+	}
+}
+bool checkOnSorted(const std::string& fileName)
+{
+	std::ifstream file(fileName);
+	if (!file.is_open()) {
+		throw("Error open file");
+		return false;
+	}
+	int value;
+	int tmp;
+	file >> tmp;
+	while (file >> value) {
+		if (tmp > value) {
+			return false;
+		}
+		tmp = value;
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
 }
