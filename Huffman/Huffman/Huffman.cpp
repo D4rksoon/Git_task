460dbdfe8815ef40590d8a7a518515ac27ea40db
D4rksoon <Asdreem2539@gmail.com>
2023-09-23 00:46:59 +0800

    Добавлен метод build

--- Huffman/Huffman/Huffman.cpp
+++ Huffman/Huffman/Huffman.cpp
@@ -1,16 +1,4 @@
-<<<<<<< Updated upstream
-﻿#include <iostream>
-#include <fstream>
 #include <Windows.h>
-=======
-<<<<<<< Updated upstream
-﻿#include <iostream>
-#include <fstream>
-#include <Windows.h>
-=======
-﻿#include <Windows.h>
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 #include "HuffmanTree.h"
 
 void Table(const std::string& text, int Tab[256])
@@ -26,25 +14,6 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	int i = 0;
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-	std::string fileName("textFile.txt");
-	std::string text("abcd");
-	std::ofstream file(fileName);
-	file << text;
-	int Tab[256] = {0};
-	Table(text, Tab);
-	for (int i = 0; i < 256; i++) {
-		std::cout << Tab[i];
-	}
-	int symb = Tab[text[0]];
-	std::cout << "\nText " << text[0] << ' ';
-	std::cout << "= " << symb << '\n';
-<<<<<<< Updated upstream
-=======
-=======
 	HuffmanTree tree;
 	//std::string fileName("textFile.txt");
 	//std::string text("aabbcdeeefff");
@@ -63,6 +32,4 @@ int main()
 	//std::cout << "= " << symb << '\n';
 
 
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 }
